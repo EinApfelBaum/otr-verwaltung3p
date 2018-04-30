afa0ae3a57a321ad1722bdd16b92a6a04896ff8f
gcurse <gcurse@web.de>
2018-04-30 20:27:37 +0200

    setup.py now works, new program icon
    otrverwaltung.desktop.in: changed titel

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# BEGIN LICENSE
+### BEGIN LICENSE
 # Copyright (C) 2010 Benjamin Elbers <elbersb@gmail.com>
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 3, as published
-# by the Free Software Foundation.
+#This program is free software: you can redistribute it and/or modify it 
+#under the terms of the GNU General Public License version 3, as published 
+#by the Free Software Foundation.
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranties of
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
-# PURPOSE.  See the GNU General Public License for more details.
+#This program is distributed in the hope that it will be useful, but 
+#WITHOUT ANY WARRANTY; without even the implied warranties of 
+#MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#PURPOSE.  See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with this program.  If not, see <http://www.gnu.org/licenses/>.
-# END LICENSE
+#You should have received a copy of the GNU General Public License along 
+#with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
 
 ###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
 import sys
@@ -30,18 +30,19 @@ import tarfile
 
 
 def update_data_path(prefix, oldvalue=None):
-    try:
-        fin = file('otrverwaltung/path.py', 'r')
-        fout = file(fin.name + '.new', 'w')
 
-        for line in fin:
-            fields = line.split(' = ')  # Separate variable from value
+    try:
+        fin = open('otrverwaltung/path.py', 'r')
+        fout = open(fin.name + '.new', 'w')
+
+        for line in fin:            
+            fields = line.split(' = ') # Separate variable from value
             if fields[0] == 'data_dir':
                 # update to prefix, store oldvalue
                 if not oldvalue:
                     oldvalue = fields[1]
                     line = "%s = '%s'\n" % (fields[0], prefix)
-                else:  # restore oldvalue
+                else: # restore oldvalue
                     line = "%s = %s" % (fields[0], oldvalue)
             fout.write(line)
 
@@ -50,17 +51,18 @@ def update_data_path(prefix, oldvalue=None):
         fin.close()
         os.rename(fout.name, fin.name)
     except (OSError, IOError) as e:
-        print("ERROR: Can't find otrverwaltung/path.py")
+        print ("ERROR: Can't find otrverwaltung/path.py")
         sys.exit(1)
     return oldvalue
 
 
 def update_desktop_file(datadir):
-    try:
-        fin = file('otrverwaltung.desktop.in', 'r')
-        fout = file(fin.name + '.new', 'w')
 
-        for line in fin:
+    try:
+        fin = open('otrverwaltung.desktop.in', 'r')
+        fout = open(fin.name + '.new', 'w')
+
+        for line in fin:            
             if 'Icon=' in line:
                 line = "Icon=%s\n" % (datadir + 'media/icon.png')
             fout.write(line)
@@ -69,31 +71,29 @@ def update_desktop_file(datadir):
         fin.close()
         os.rename(fout.name, fin.name)
     except (OSError, IOError) as e:
-        print("ERROR: Can't find otrverwaltung.desktop.in")
+        print ("ERROR: Can't find otrverwaltung.desktop.in")
         sys.exit(1)
 
-
 def pack_tools_dir():
+
     if os.path.isfile('data/tools.tar.bz2'):
         os.remove('data/tools.tar.bz2')
-
+        
     def reset(tarinfo):
         tarinfo.uid = tarinfo.gid = 0
         tarinfo.uname = tarinfo.gname = "root"
         return tarinfo
-
+        
         tar = tarfile.open("data/tools.tar.bz2", "w:bz2")
         tar.add("data/tools", filter=reset)
         tar.close()
-
-
+    
 class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
     def run(self):
-        self.prefix = '/usr'
+        self.prefix='/usr'
         if self.root or self.home:
-            print("WARNING: You don't use a standard --prefix installation, take care that you eventually " \
-                  "need to update quickly/quicklyconfig.py file to adjust __quickly_data_directory__. You can " \
-                  "ignore this warning if you are packaging and uses --prefix.")
+            print("WARNING: You don't use a standard --prefix installation. You can " \
+            "ignore this warning if you are packaging and use --prefix.")
         previous_value = update_data_path(self.prefix + '/share/otrverwaltung/')
         update_desktop_file(self.prefix + '/share/otrverwaltung/')
         pack_tools_dir()
@@ -101,6 +101,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
         update_data_path(self.prefix, previous_value)
 
 
+        
 ##################################################################################
 ###################### YOU SHOULD MODIFY ONLY WHAT IS BELOW ######################
 ##################################################################################
@@ -113,6 +114,6 @@ DistUtilsExtra.auto.setup(
     author_email='elbersb@gmail.com',
     description='Dateien von onlinetvrecorder.com verwalten',
     long_description='Dateien von onlinetvrecorder.com verwalten: Schneiden, Schnitte betrachten, Cutlists bewerten...',
-    url='http://github.com/gcurse/otr-verwaltung3p',
-    cmdclass={'install': InstallAndUpdateDataDirectory}
-)
+    url='http://github.com/elbersb/otr-verwaltung',
+    cmdclass={'install': InstallAndUpdateDataDirectory}  
+    )
